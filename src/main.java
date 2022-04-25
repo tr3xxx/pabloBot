8f1963dcb7ab80e5d92984d5801e4cdfc992be1c
tr3x <leon.burghardt99@gmail.com>
2022-04-25 18:11:41 +0200

    main added

--- src/main.java
+++ src/main.java
@@ -1,1 +1,21 @@
-class Hallo{}
+public class ReadyListener implements EventListener
+{
+    public static void main(String[] args)
+            throws LoginException, InterruptedException
+    {
+        // Note: It is important to register your ReadyListener before building
+        JDA jda = JDABuilder.createDefault("OTY4MTgxNjQ0MTIzMDQ1OTk4.YmbHaw.SX-6v3cNdA-R7LcMEZwFZfnZcxE")
+                .addEventListeners(new ReadyListener())
+                .build();
+
+        // optionally block until JDA is ready
+        jda.awaitReady();
+    }
+
+    @Override
+    public void onEvent(GenericEvent event)
+    {
+        if (event instanceof ReadyEvent)
+            System.out.println("API is ready!");
+    }
+}
