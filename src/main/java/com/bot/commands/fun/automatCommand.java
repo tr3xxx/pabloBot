dd994a9f6ac8db8139361b6f755cee2765f8a05b
Kylx2128 <robin.eckebracht@gmx.de>
2022-04-26 18:11:45 +0200

    fun-Ordner und automat Klasse

---
+++ src/main/java/com/bot/commands/fun/automatCommand.java
@@ -0,0 +1,23 @@
+package com.bot.commands.fun;
+
+import com.bot.commands.core.Command;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class automatCommand extends Command {
+    @Override
+    public String call() {
+        return "Automat";
+    }
+
+    @Override
+    public String help() {
+        return null;
+    }
+
+    @Override
+    public boolean execute(String[] args, MessageReceivedEvent event) {
+
+        return false;
+    }
+
+}
