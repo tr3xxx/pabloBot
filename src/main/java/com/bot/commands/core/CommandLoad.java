b43ffb0304972db0c96c78776c582ba887a4689a
Kylx2128 <robin.eckebracht@gmx.de>
2022-04-28 18:50:53 +0200

    automat fertig

--- src/main/java/com/bot/commands/core/CommandLoad.java
+++ src/main/java/com/bot/commands/core/CommandLoad.java
@@ -1,6 +1,7 @@
 package com.bot.commands.core;
 
 import com.bot.commands.basic.testCommand;
+import com.bot.commands.fun.automatCommand;
 import com.bot.commands.voice.voicehub.setVoicehub;
 import net.dv8tion.jda.api.JDA;
 
@@ -8,5 +9,6 @@ public class CommandLoad {
     public CommandLoad(JDA jda){
         CommandManager.addCommand(new testCommand());
         CommandManager.addCommand(new setVoicehub());
+        CommandManager.addCommand(new automatCommand());
     }
 }
